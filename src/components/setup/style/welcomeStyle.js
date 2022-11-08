25eafa6d2ee4cc00f22811dbe2d646ec7e6e71d5
kkt9102 <kkt9102@gmail.com>
2022-11-08 18:59:05 +0900

    setup Page element setup

--- src/components/setup/style/welcomeStyle.js
+++ src/components/setup/style/welcomeStyle.js
@@ -1,7 +1,13 @@
 import styled from "styled-components";
-
+// color:#6656ff;
 export const WelcomeSection = {
   WelcomeFrame:styled.div`
     display:flex; justify-content:center; align-items: center;
+    button {background:#6656ff; color:#ffffff;}
+    .has_acc {
+    	p {
+    		div {color:#6656ff;}
+    	}
+    }
   `
 }
--- src/components/setup/view/WelcomePage.jsx
+++ src/components/setup/view/WelcomePage.jsx
@@ -5,6 +5,12 @@ const WelcomePage = () => {
   return(
     <WelcomeSection.WelcomeFrame>
       <img src={images.welcomeLogo} alt="" />
+      <div className="flex flex_dir_c">
+    	<button>시작하기</button>
+    	<div className="has_acc flex">
+    		<p className="flex">이미 계정이 있나요?<div className="cursor_p">로그인</div></p>
+    	</div>
+      </div>
     </WelcomeSection.WelcomeFrame>
   )
 }
