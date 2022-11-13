f9b0e449a0affa7eacfd37f652e320dd374ca193
kkt9102 <kkt9102@gmail.com>
2022-11-13 23:01:43 +0900

    사용자 접속 좌표위치 이동은 성공, 카카오맵 API 중첩 랜더링문제 해결실패

--- src/components/main/view/map/Map.js
+++ src/components/main/view/map/Map.js
@@ -1,87 +1,45 @@
 import React, { useEffect, useState } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const KakaoMapAPI = () => {
-//   function success({ coords, timestamp }) {
-//     const latitude = coords.latitude;   // 위도
-//     const longitude = coords.longitude; // 경도
-    
-//     console.log(`위도: ${latitude}, 경도: ${longitude}, 위치 반환 시간: ${timestamp}`);
-// }
-
-// function getUserLocation() {
-//     if (!navigator.geolocation) {
-//         throw "위치 정보가 지원되지 않습니다.";
-//     }
-//     navigator.geolocation.watchPosition(success);
-// }
-// getUserLocation();
-// const realtime = ({ coords, timestamp}) => {
-//   const latitude = coords.latitude;   // 위도
-//   const longitude = coords.longitude; // 경도
-// }
-
-
-  const [map,setMap] = useState(null);
-  // navigator.geolocation.getCurrentPosition(function (position) {
-  //   map.panTo(
-  //       new window.kakao.maps.LatLng(
-  //           position.coords.latitude,
-  //           position.coords.longitude
-  //       )
-  //   );
-  // });
   useEffect(()=>{
-      const container = document.getElementById('map');
-      const options = { 
-        center: new window.kakao.maps.LatLng(
-          33.450701, 
-          126.570667
-        ),
-      };
-      
-      const kakaoMap = new window.kakao.maps.Map(container, options);
-      setMap(kakaoMap);
-
-      // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-      if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-          
-          let lat = position.coords.latitude, // 위도
-              lon = position.coords.longitude; // 경도
-          let locPosition = new window.kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-          
-          // 마커와 인포윈도우를 표시합니다
-          displayMarker(locPosition);
-              
-        });
-      
-    } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-      
-      let locPosition = new window.kakao.maps.LatLng(33.450701, 126.570667),    
-          message = 'geolocation을 사용할수 없어요..'
-      displayMarker(locPosition,message);
+    const container = document.getElementById("maps");
+    const options = { 
+      center: new window.kakao.maps.LatLng(
+        33.450701, 
+        126.570667
+      ),
+    };
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        map.panTo(
+          new window.kakao.maps.LatLng(
+              position.coords.latitude,
+              position.coords.longitude
+          )
+        );
+      });
     }
-
-    // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-    function displayMarker(locPosition, message) {
-      // 마커를 생성합니다
-      let marker = new window.kakao.maps.Marker({  
-          map: kakaoMap, 
-          position: locPosition
-      }); 
-      let iwContent = message, // 인포윈도우에 표시할 내용
-          iwRemoveable = true;
-      // 지도 중심좌표를 접속위치로 변경합니다
-      kakaoMap.setCenter(locPosition);      
-    }
-
+    const map = new window.kakao.maps.Map(container, options);
   },[]);
+
+ 
   
   return (
-    <div>
-      <div id="map" style={{width:"100%", height:"100vh"}}></div> 
-    </div>
-      )
+      <div>
+        <div id="maps" style={{width:"100%", height:"100vh"}}></div>
+        {/* <Map center={{
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+          style={{
+            width: "100%", 
+            height: "100vh"
+          }}
+        >
+        </Map> */}
+      </div>
+    )
   }
 
 export default KakaoMapAPI;
