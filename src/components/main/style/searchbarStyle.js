f9ea2fd13b46ab4a8698b67d9aa6129ca3adeb3c
kkt9102 <kkt9102@gmail.com>
2022-11-13 17:10:20 +0900

    카카오맵 API 중첩 랜더링문제 발생

--- src/components/main/style/searchbarStyle.js
+++ src/components/main/style/searchbarStyle.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const SearchBarSection = {
   SearchFrame:styled.div`
-    position:absolute; display: flex; justify-content: space-between; align-items: center; flex-basis:100%; width:100%; top:0; left:0; padding:0.8rem 0; background:#ffffff; z-index:5;
+    position:absolute; display: flex; justify-content: space-between; align-items: center; flex-basis:100%; width:100%; top:0; left:0; padding:0.8rem 2rem; background:#ffffff; z-index:5;
     .bars_btn {flex-basis:1.8rem; height:1.2rem;
       > div {position:absolute; width:100%; height:0.2rem; border-radius:0.5rem; background:#959292;
         :first-child {top:0;}
