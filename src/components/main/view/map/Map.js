61a659448f94ef5a74c20f24a6089fe31555babc
kkt9102 <kkt9102@gmail.com>
2022-11-14 23:10:49 +0900

    kakaomap maker click move page

--- src/components/main/view/map/Map.js
+++ src/components/main/view/map/Map.js
@@ -1,27 +1,60 @@
 import React, { useEffect, useState } from "react";
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { useNavigate } from "react-router-dom";
+// import { Map, MapMarker } from "react-kakao-maps-sdk";
+import marker_c from "../../../../resources/img/logo/marker_c.svg";
+import { MapMark } from "./MapMaker";
+
 
 const KakaoMapAPI = () => {
+  const navigate = useNavigate();
   useEffect(()=>{
-    // const container = document.getElementById("maps");
-    // const options = { 
-    //   center: new window.kakao.maps.LatLng(
-    //     33.450701, 
-    //     126.570667
-    //   ),
-    // };
-    // if ("geolocation" in navigator) {
-    //   navigator.geolocation.getCurrentPosition(function (position) {
-    //     map.panTo(
-    //       new window.kakao.maps.LatLng(
-    //           position.coords.latitude,
-    //           position.coords.longitude
-    //       )
-    //     );
-    //   });
-    // }
-    // const map = new window.kakao.maps.Map(container, options);
-  },[]);
+    const container = document.getElementById('maps');
+    // const lat = navigator.geolocation.getCurrentPosition(document.coords.latitude);
+    // const lon = navigator.geolocation.getCurrentPosition(document.coords.longitude);
+    const options = {
+      center: new window.kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+      // center : new window.kakao.maps.LatLng(lat,lon),
+      level: 7,
+      
+    };
+    const map = new window.kakao.maps.Map(container, options);
+        
+    const infos = [];
+    MapMark.forEach((reserve) => {
+    // 커스텀 마커
+      const imageSrc = marker_c,
+      imageSize = new window.kakao.maps.Size(27,46);
+      const reserveMaker = new window.kakao.maps.MarkerImage(imageSrc,imageSize);
+  
+      const custom_marker = new window.kakao.maps.Marker({
+        map: map,
+        position: new window.kakao.maps.LatLng(reserve.lat, reserve.lng),
+        image: reserveMaker,
+      });
+  
+      // infoWindow용
+      const iwContent = reserve.content
+  
+      const infowindow = new window.kakao.maps.InfoWindow({
+          content : iwContent,
+          removable : true,
+      });
+  
+      infos.push(infowindow);
+  
+      let openInfo = function() {
+        infos.forEach(function(each) { each.close(); }); // close all infos
+        infowindow.open(map, custom_marker); // open target info
+      };
+  
+      let goDdetailPage = () => {
+        navigate("/regist");
+      }
+      window.kakao.maps.event.addListener(custom_marker, 'click', goDdetailPage);
+    });
+    }, 
+  [])
+  
 
   
   return (
---
+++ src/components/main/view/map/MapMaker.js
@@ -0,0 +1,11 @@
+export const MapMark = [
+  {
+    // center: new window.kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
+    lat: 37.365264512305174, 
+    lng: 127.10676860117488
+  }
+];
+
+export const CustomMarker = [{
+  image:'../resource/img/map_mark.png'
+}];
