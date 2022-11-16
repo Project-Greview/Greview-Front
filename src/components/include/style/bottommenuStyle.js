5858ea2dd64a9e515ba24265c7c57bc19ca42651
kkt9102 <kkt9102@gmail.com>
2022-11-17 00:36:46 +0900

    bottom Menu Margin add

--- src/components/include/style/bottommenuStyle.js
+++ src/components/include/style/bottommenuStyle.js
@@ -6,7 +6,9 @@ export const BottomMenuSection = {
     position: fixed; width: 100%; height: 8rem; left:0; bottom:0; background: #ffffff; border-top:0.1rem solid #D0CFCF; z-index: 10;
     ul {height:inherit;
       li {
-        div {font-size:1rem; font-weight:500;}
+        div {font-size:1rem; font-weight:500;
+          &.img_box {margin-bottom:10px;}
+        }
       }
     }
   `
