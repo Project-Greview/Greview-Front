5c84cc2e9e1a1b7c1f773805584ce74f4a62fb18
Suass <tjdgml1311@gmail.com>
2022-12-01 22:17:47 +0900

    검색 결과 페이지 추가

---
+++ src/components/home/view/SearchResult.jsx
@@ -0,0 +1,67 @@
+import { SearchResultSection } from "../style/searchResultStyle";
+import { Common } from "../../../resources/style/common/commonStyle";
+
+const SearchResult = () => {
+  
+  return(
+    <SearchResultSection.ListFrame>
+      <div className="tab">
+        <ul>
+          <li className="on">리뷰</li>
+          <li>장소명</li>
+        </ul>
+      </div>
+      <div className="result_list">
+        <div className="result_num flex">
+          <p><strong>5개</strong>의 검색결과가 있습니다.</p>
+          <select name="" id="">
+            <option value="">최신순</option>
+            <option value="">추천순</option>
+          </select>
+        </div>
+        <ul>
+          <li>
+            <div className="img"></div>
+            <div className="info">
+              <p><strong>제일곱창</strong>5km</p>
+              <p className="review">
+                곱창이 맛없을 수 없지만 여긴 정말 맛있습니다. 2시간 반 <strong>웨이팅</strong>을 했지만 먹으니
+              </p>
+              <div className="profile">
+                <div className="pro_img"></div>
+                <p>먹짱</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="img"></div>
+            <div className="info">
+              <p><strong>제일곱창</strong>5km</p>
+              <p className="review">
+                곱창이 맛없을 수 없지만 여긴 정말 맛있습니다. 2시간 반 <strong>웨이팅</strong>을 했지만 먹으니
+              </p>
+              <div className="profile">
+                <div className="pro_img"></div>
+                <p>먹짱</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="info no_img">
+              <p><strong>제일곱창</strong>5km</p>
+              <p className="review">
+                곱창이 맛없을 수 없지만 여긴 정말 맛있습니다. 2시간 반 <strong>웨이팅</strong>을 했지만 먹으니 곱창이 맛없을 수 없지만 여긴 정말 맛있습니다. 2시간 반 웨이팅을 했지만 먹으니
+              </p>
+              <div className="profile">
+                <div className="pro_img"></div>
+                <p>먹짱</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </SearchResultSection.ListFrame>
+  )
+}
+
+export default SearchResult;
